604bf1b8fade4ce59ae7dbc6eb546b74ab4d6128
AgustinHV <programador.agustinhv@gmail.com>
2024-04-10 17:55:57 -0600

    feat:https heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,27 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
      */
-    public function register(): void
+    public function register()
     {
-        //
+       if (env('REDIRECT_HTTPS')) {
+           $this->app['request']->server->set('HTTPS', true);
+       }
     }
 
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot(UrlGenerator $url)
     {
-        //
+        if (env('REDIRECT_HTTPS')) {
+            $url->formatScheme('https://');
+        }
     }
 }
